6dff54cb15ce89ddb9ed1746bd8e5658bceee9d9
naveen@galaxy4 <naveenacad89@gmail.com>
2025-04-18 17:42:39 +0000

    initial-commit

---
+++ app/components/Token.js
@@ -0,0 +1,7 @@
+import { ethers } from "ethers";
+
+function Token({ toggleTrade, token }) {
+  return <button onClick={() => toggleTrade(token)} className="token"></button>;
+}
+
+export default Token;
