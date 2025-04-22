c75a92052d5b3a1c1ae1afe83b4caf6b91190d9c
naveen@galaxy4 <naveenacad89@gmail.com>
2025-04-22 02:10:30 +0000

    add func

--- app/components/List.js
+++ app/components/List.js
@@ -1,10 +1,12 @@
-import { ethers } from "ethers"
+import { ethers } from 'ethers';
 
 function List({ toggleCreate, fee, provider, factory }) {
-
   return (
     <div className="list">
-
+      <p>create token</p>
+      <button onClick={toggleCreate} className="btn--fancy">
+        [cancel]
+      </button>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,26 +1,64 @@
-"use client"
+'use client';
 
-import { useEffect, useState } from "react"
-import { ethers } from 'ethers'
+import { Profiler, useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
 // Components
-import Header from "./components/Header"
-import List from "./components/List"
-import Token from "./components/Token"
-import Trade from "./components/Trade"
+import Header from './components/Header';
+import List from './components/List';
+import Token from './components/Token';
+import Trade from './components/Trade';
 
 // ABIs & Config
-import Factory from "./abis/Factory.json"
-import config from "./config.json"
-import images from "./images.json"
+import Factory from './abis/Factory.json';
+import config from './config.json';
+import images from './images.json';
 
 export default function Home() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [factory, setFactory] = useState(null);
+  const [fee, setFee] = useState(0);
+  const [showCreate, setShowCreate] = useState(false);
+
+  function toggleCreate() {
+    setShowCreate(!showCreate);
+  }
+
+  async function loadBlockchainData() {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+
+    const factory = new ethers.Contract(config[network.chainId].factory.address, Factory, provider);
+    setFactory(factory);
+
+    const fee = await factory.fee();
+  }
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div className="page">
+      <Header account={account} setAccount={setAccount} />
+      <main>
+        <div className="create">
+          <button onClick={factory && account && toggleCreate} className="btn--fancy">
+            {!factory
+              ? '[contract not deployed]'
+              : !account
+              ? '[please connect]'
+              : '[start a new token]'}
+          </button>
+        </div>
+      </main>
 
-      <h1 style={{ padding: "1em" }}>fun.pump</h1>
-
+      {showCreate && (
+        <List toggleCreate={toggleCreate} fee={fee} provider={provider} factory={factory} />
+      )}
     </div>
   );
 }
