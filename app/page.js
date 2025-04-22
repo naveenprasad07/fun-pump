a2da79b3e559653cc14322dfa6622a0b7745275d
naveen@galaxy4 <naveenacad89@gmail.com>
2025-04-22 03:26:37 +0000

    final-commit

--- app/page.js
+++ app/page.js
@@ -19,10 +19,18 @@ export default function Home() {
   const [account, setAccount] = useState(null);
   const [factory, setFactory] = useState(null);
   const [fee, setFee] = useState(0);
+  const [tokens, setTokens] = useState([]);
+  const [token, setToken] = useState(null);
   const [showCreate, setShowCreate] = useState(false);
+  const [showTrade, setShowTrade] = useState(false);
 
   function toggleCreate() {
     setShowCreate(!showCreate);
+  }
+
+  function toggleTrade(token) {
+    setToken(token);
+    showTrade ? setShowTrade(false) : setShowTrade(true);
   }
 
   async function loadBlockchainData() {
@@ -35,6 +43,31 @@ export default function Home() {
     setFactory(factory);
 
     const fee = await factory.fee();
+    setFee(fee);
+
+    const totalTokens = await factory.totalTokens();
+    const tokens = [];
+
+    for (let i = 0; i < totalTokens; i++) {
+      if (i == 6) {
+        break;
+      }
+
+      const tokenSale = await factory.getTokenSale(i);
+
+      const token = {
+        token: tokenSale.token,
+        name: tokenSale.name,
+        creator: tokenSale.creator,
+        sold: tokenSale.sold,
+        raised: tokenSale.raised,
+        isOpen: tokenSale.isOpen,
+        image: images[i],
+      };
+
+      tokens.push(token);
+    }
+    setTokens(tokens.reverse());
   }
 
   useEffect(() => {
@@ -54,10 +87,28 @@ export default function Home() {
               : '[start a new token]'}
           </button>
         </div>
+
+        <div className="listings">
+          <h1>new listings</h1>
+          <div className="tokens">
+            {!account ? (
+              <p>please connect wallet</p>
+            ) : tokens.length === 0 ? (
+              <p>No tokens listed</p>
+            ) : (
+              tokens.map((token, index) => (
+                <Token toggleTrade={toggleTrade} token={token} key={index} />
+              ))
+            )}
+          </div>
+        </div>
       </main>
 
       {showCreate && (
         <List toggleCreate={toggleCreate} fee={fee} provider={provider} factory={factory} />
+      )}
+      {showTrade && (
+        <Trade toggleTrade={toggleTrade} token={token} provider={provider} factory={factory} />
       )}
     </div>
   );
